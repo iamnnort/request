dff970f32f406782f98c344e1062c5c536e07e60
Nikita Pavets <nikitapavets@gmail.com>
2023-10-11 11:48:26 +0000

    add data source

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,64 @@ export const request = (baseRequestConfig: BaseRequestConfig) => {
   };
 };
 
+export const makeDataSource = <T, SP, SR, C, U>(
+  baseRequestConfig: BaseRequestConfig
+) => {
+  const dataSourceRequest = request(baseRequestConfig);
+
+  abstract class DataSource {
+    public static url: string;
+
+    public static async search(request: SP = {} as SP) {
+      return dataSourceRequest<SR>({
+        method: methods.GET,
+        ...request,
+      });
+    }
+
+    public static async get(id: any, request: SP = {} as SP) {
+      return dataSourceRequest<T>({
+        method: methods.GET,
+        url: `/${id}`,
+        ...request,
+      });
+    }
+
+    public static async create(request: C = {} as C) {
+      return dataSourceRequest<T>({
+        method: methods.POST,
+        ...request,
+      });
+    }
+
+    public static async bulkCreate(request: C = {} as C) {
+      return dataSourceRequest<T>({
+        method: methods.POST,
+        url: "/bulk",
+        ...request,
+      });
+    }
+
+    public static async update(id: any, request: U = {} as U) {
+      return dataSourceRequest<T>({
+        method: methods.PUT,
+        url: `/${id}`,
+        ...request,
+      });
+    }
+
+    public static async remove(id: any, request: SP = {} as SP) {
+      return dataSourceRequest<T>({
+        method: methods.DELETE,
+        url: `/${id}`,
+        ...request,
+      });
+    }
+  }
+
+  return DataSource;
+};
+
 export const methods = config.methods;
 export const statuses = config.statuses;
 
